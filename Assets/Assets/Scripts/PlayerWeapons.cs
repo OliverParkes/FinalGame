b84ab3c149345c802bbf43ebc320270b4ffbd012
OliverParkes <oliverparkes@hotmail.com>
2020-10-26 20:06:10 +1030

    New version of game

---
+++ Assets/Assets/Scripts/PlayerWeapons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerWeapons
+{
+    public string rifleName = "M107";
+
+    public float damage = 100f;
+    public float range = 600;
+    public float weight = 1.5f;
+    public float magazine = 10;
+
+   
+
+}
---
+++ Assets/Assets/Scripts/SemiAutomatic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SemiAutomatic : MonoBehaviour
+{
+    
+    public PlayerWeapons weapon;
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    public LayerMask mask;
+    void Start()
+    {
+        if(cam == null)
+        {
+            Debug.LogError("No Camera: semiAutomatic");
+            this.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("fired");
+            Shoot();
+        }
+    }
+    
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+        {
+            if (hit.collider.tag == "RemotePlayer")
+            {
+                CmdPlayerShot(hit.collider.name);
+            }
+        }
+    }
+
+    
+    void CmdPlayerShot(string ID)
+    {
+        Debug.Log(ID + "has been shot");
+    }
+}
