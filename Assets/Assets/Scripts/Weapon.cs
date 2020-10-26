8440d808bace4b4919ffe55bdf316a7f1e2ada26
OliverParkes <oliverparkes@hotmail.com>
2020-10-26 20:30:07 +1030

    Added weapons

---
+++ Assets/Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float damage;
+    public float range;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    }
+}
